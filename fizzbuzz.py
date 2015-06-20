a14a07a0b2893e6870f76c0f186f4e187a3c4318
YuZakuro <zakuro@yuzakuro.me>
2015-06-21 08:49:52 +0900

    py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,1 @@
-print(' '.join([(''if i%3else'fizz')+(''if i%5else'buzz')+(str(i)if i%3and i%5 else'')for i in range(1,101)]))
+print(' '.join([(''if i%3else'fizz')+(''if i%5else'buzz')or str(i)for i in range(1,101)]))
